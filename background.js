f93cb7ef055594e587eaf0aa61889178fe994074
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-08-02 21:18:51 -0400

    Update: Save current Tab

--- background.js
+++ background.js
@@ -4,11 +4,16 @@ chrome.action.onClicked.addListener((tab) => {
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: () => { alert("clieckr me")}
+        func: (url) => { 
+            alert("You clicked me")
+            alert("Got tab" + url)
+            let bookmark = bookmarks.create({
+                title: "bookmarks.create() on MDN",
+                url: tab.url,
+                
+            }).then(console.log(node))
+            chrome.runtime.sendMessage({ type: 'createBookmark', url });
+        },
+        args: [tab.url]
     });
 });
-
-
-// chrome.action.onClicked.addListener((tab) => {
-//   console.log("Extension icon clicked!");
-// });
