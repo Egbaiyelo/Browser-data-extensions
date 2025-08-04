7c080ce892d0a44165e6be826dfc8d2e85778eb6
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-08-04 08:23:12 -0400

    Update: Async Data Response, Name and Todo
    Changing background.js to respond asynchronously
    Changing name to browser-data

--- count-tabs/TODO.md
+++ count-tabs/TODO.md
@@ -11,4 +11,7 @@ Number bookmarks
 <!-- More complex -->
 Time spent on current tab
 Time spent on current window
-Memory used on tabs
+
+Memory used on tabs - like the way google does
+
+Number of visited sites per day
--- count-tabs/background.js
+++ count-tabs/background.js
@@ -1,75 +1,44 @@
-
-chrome.action.onClicked.addListener(() => {
-
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     let data = {
-        "windows": 0,
-        "tabs": 0,
-
-        "current_window_tabs": 0,
-
-        "incognito_windows": 0,
-        "incognito_tabs": 0,
-
-        "bookmarks": 0,
-    }
+        windows: 0,
+        tabs: 0,
+        current_window_tabs: 0,
+        incognito_windows: 0,
+        incognito_tabs: 0,
+        bookmarks: 0,
+    };
 
     // Handle windows
     chrome.windows.getAll({}, (windows) => {
         data.windows = windows.length;
-        console.log("Total open windows:", data.windows);
+        data.incognito_windows = windows.filter(win => win.incognito).length;
 
-        let incognito_windows = 0;
-        windows.forEach(window => {
-            if (window.incognito) {
-                incognito_windows++;
-            }
+        // Handle tabs
+        chrome.tabs.query({}, (tabs) => {
+            data.tabs = tabs.length;
+            data.incognito_tabs = tabs.filter(tab => tab.incognito).length;
+
+            // Handle current window
+            chrome.windows.getCurrent({ populate: true }, (currentWindow) => {
+                data.current_window_tabs = currentWindow?.tabs?.length || 0;
+
+                // Handle bookmarks
+                chrome.bookmarks.getTree((bookmarkTreeNodes) => {
+                    let count = 0;
+                    function countBookmarks(nodes) {
+                        nodes.forEach(node => {
+                            if (node.url) count++;
+                            if (node.children) countBookmarks(node.children);
+                        });
+                    }
+                    countBookmarks(bookmarkTreeNodes);
+                    data.bookmarks = count;
+
+                    sendResponse(data); 
+                });
+            });
         });
-
-        data.incognito_windows = incognito_windows;
-        console.log("Total incognito windows:", data.incognito_windows);
     });
 
-    // Handle tabs
-    chrome.tabs.query({}, (tabs) => {
-        data.tabs = tabs.length;
-        console.log("Total open tabs:", data.tabs);
-
-        let incognito_tabs = 0;
-        tabs.forEach(tab => {
-            if (tab.incognito) {
-                incognito_tabs++;
-            }
-        })
-
-        data.incognito_tabs = incognito_tabs;
-        console.log("Total incognito tabs:", data.incognito_tabs);
-    })
-
-    // Handle current windows tab
-    //- propably add to above?
-    chrome.windows.getCurrent({ populate: true }, (currentWindow) => {
-        if (currentWindow) {
-            data.current_window_tabs = currentWindow.tabs ? currentWindow.tabs.length : 0;
-            console.log("Current window tab count:", data.current_window_tabs);
-        }
-    });
-
-    // Handle bookmarks
-    chrome.bookmarks.getTree((bookmarkTreeNodes) => {
-        console.log("doing")
-        let count = 0;
-        function countBookmarks(nodes) {
-            for (let node of nodes) {
-                if (node.url) {
-                    count++;
-                }
-                if (node.children) {
-                    countBookmarks(node.children);
-                }
-            }
-        }
-        countBookmarks(bookmarkTreeNodes);
-        console.log("Total bookmarks:", count);
-        console.log("Bookmarks API:", chrome.bookmarks);
-    });
-})
+    return true; 
+});
