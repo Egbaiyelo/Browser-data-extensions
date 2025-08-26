76386b4e35b4e5fd1eabcb2f9be042090c56dbac
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-08-25 22:44:19 -0400

    Update popup.js

--- browser-data/src/popup.js
+++ browser-data/src/popup.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     settings_button.addEventListener('click', () => {
         settings_page.style.display = 'block';
-        main_page.style.display = 'none';
+        main_page.style.display  = 'none';
     });
 });
 
