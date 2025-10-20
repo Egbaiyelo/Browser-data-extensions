ef18e8172674dfc9ea819372359a9f9c8fe3566b
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-10-20 09:26:14 -0400

    Chore: Cleanup listeners
    Removing some logic

--- src/background.js
+++ src/background.js
@@ -35,16 +35,16 @@ async function getData() {
 
 
     // Handle bookmarks
-    const bookmarkTreeNodes = await new Promise(resolve => chrome.bookmarks.getTree(resolve));
-    let count = 0;
-    function countBookmarks(nodes) {
-        nodes.forEach(node => {
-            if (node.url) count++;
-            if (node.children) countBookmarks(node.children);
-        });
-    }
-    countBookmarks(bookmarkTreeNodes);
-    data.bookmarks = count;
+    // const bookmarkTreeNodes = await new Promise(resolve => chrome.bookmarks.getTree(resolve));
+    // let count = 0;
+    // function countBookmarks(nodes) {
+    //     nodes.forEach(node => {
+    //         if (node.url) count++;
+    //         if (node.children) countBookmarks(node.children);
+    //     });
+    // }
+    // countBookmarks(bookmarkTreeNodes);
+    // data.bookmarks = count;
 
 
     // Get tab title 
@@ -62,36 +62,24 @@ async function getData() {
 getData();
 
 
-// --- Subscribing listeners ---
-// On popup message
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action == "getData") {
-        getData().then(data => {
-            sendResponse(data);
-        })
-        return true;
-    }
-    if (message.action === "updateSettings") {
-        if (message.type === "badgeBg") chrome.action.setBadgeBackgroundColor({ color: message.color });
-        if (message.type === "badgeText") chrome.action.setBadgeTextColor({ color: message.color });
-    }
-    if (message.action === "log") {
-        console.log(message.log);
-        console.log(message.content);
-    }
-});
 
-// On browser startup and install
-chrome.runtime.onStartup.addListener(getData) // sets badge
-chrome.runtime.onInstalled.addListener(() => {
-    getData();
-    
-    chrome.action.setBadgeBackgroundColor({ color: '"#FFFF00"' });
-    chrome.action.setBadgeTextColor({ color: '#000000' });
-    chrome.storage.sync.set({ badgeBgColor: "#FFFF00", badgeTextColor: "#000000" });
-});
-// 
-chrome.action.onClicked.addListener(getData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // --- Listening for badge changes ---
@@ -145,22 +133,85 @@ chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
 
 
 
+
+
+
+// ******************
+// *** Subscribing Listeners ***
+
+// on browser startup and install
+chrome.runtime.onInstalled.addListener(async () => {
+    console.log("Extension Installed");
+
+    // Setting default styles
+    chrome.action.setBadgeBackgroundColor({ color: '#FFFF00' });
+    chrome.action.setBadgeTextColor({ color: '#000000' });
+    chrome.storage.sync.set({ badgeBgColor: "#FFFF00", badgeTextColor: "#000000" });
+
+    await getData();
+    // await updateHeavyStats();
+});
+chrome.runtime.onStartup.addListener(getData);
+chrome.action.onClicked.addListener(getData);
+
+
+// Update data when tabs or windows change
+chrome.tabs.onCreated.addListener(getData);
+chrome.tabs.onRemoved.addListener(getData);
+chrome.windows.onCreated.addListener(getData);
+chrome.windows.onRemoved.addListener(getData);
+
+// Handling messages from Popup
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "getData") {
+        getData().then(updatedData => sendResponse(updatedData));
+        return true; 
+    }
+
+    if (message.action === "updateSettings") {
+        if (message.type === "badgeBg") chrome.action.setBadgeBackgroundColor({ color: message.color });
+        if (message.type === "badgeText") chrome.action.setBadgeTextColor({ color: message.color });
+    }
+});
+
+// update bookmarks
+// chrome.bookmarks.onCreated.addListener(updateHeavyStats);
+// chrome.bookmarks.onRemoved.addListener(updateHeavyStats);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Get history from last 24 hours
-    const oneDayAgo = Date.now() - (24 * 60 * 60 * 1000);
-    const historyItems = await new Promise(resolve =>
-        chrome.history.search({ text: "", startTime: oneDayAgo, maxResults: 10000 }, resolve)
-    );
+    // const oneDayAgo = Date.now() - (24 * 60 * 60 * 1000);
+    // const historyItems = await new Promise(resolve =>
+    //     chrome.history.search({ text: "", startTime: oneDayAgo, maxResults: 10000 }, resolve)
+    // );
 
-    const visitedDomains = new Set();
-    historyItems.forEach(entry => {
-        try {
-            const url = new URL(entry.url);
-            visitedDomains.add(url.hostname);
-        } catch (e) {
-            console.warn("Invalid URL skipped:", entry.url);
-        }
-    });
+    // const visitedDomains = new Set();
+    // historyItems.forEach(entry => {
+    //     try {
+    //         const url = new URL(entry.url);
+    //         visitedDomains.add(url.hostname);
+    //     } catch (e) {
+    //         console.warn("Invalid URL skipped:", entry.url);
+    //     }
+    // });
 
-    // console.log(`Visited domains today: ${visitedDomains.size}`);
-    // console.log(Array.from(visitedDomains));
-    // console.log(Array.from(historyItems));
+    // // console.log(`Visited domains today: ${visitedDomains.size}`);
+    // // console.log(Array.from(visitedDomains));
+    // // console.log(Array.from(historyItems));
